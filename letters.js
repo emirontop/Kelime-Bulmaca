4fa842449dd1cd75283c575cc40285e2e57bfe3c
emirontop <2fanisikeyim@gmail.com>
2025-08-01 11:57:49 +0300

    Create letters.js

---
+++ letters.js
@@ -0,0 +1,24 @@
+const letters = {
+  "Hello": "Merhaba",
+  "Bye": "Baybay",
+  "Dog": "Köpek",
+  "Cat": "Kedi",
+  "Car": "Araba",
+  "Computer": "Bilgisayar",
+  "Keyboard": "Klavye",
+  "Mouse": "Fare",
+  "Book": "Kitap",
+  "Phone": "Telefon",
+  "Window": "Pencere",
+  "Chair": "Sandalye",
+  "Table": "Masa",
+  "Sun": "Güneş",
+  "Moon": "Ay",
+  "Star": "Yıldız",
+  "Water": "Su",
+  "Fire": "Ateş",
+  "Earth": "Toprak",
+  "Sky": "Gökyüzü"
+};
+
+export default letters;
